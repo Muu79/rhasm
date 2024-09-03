4df608431ac48d87aac39fc57f9f1e8c8323667a
muu <muu794@gmail.com>
2024-09-03 21:52:27 +0200

    implemented clap for cli

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-use std::{ env, io };
+use std::{ env, io, path::PathBuf };
 use rhasm::Assembler;
+use clap::{ Parser, ArgAction };
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "rhasm",
+    version = "0.1.1",
+    about = "A simple assembler/disassembler for the Hack computer from the Nand2Tetris course",
+    author = "Muaaz Bhyat muu794@gmail.com"
+)]
+struct Cli {
+    /// The input file to read from
+    #[arg(required = true)]
+    in_file_path: PathBuf,
+
+    /// The output file to write to
+    #[arg(short, long)]
+    out_file_path: Option<PathBuf>,
+
+    /// Disassemble the input file
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    disassemble: bool,
+}
 
 fn main() -> io::Result<()> {
-    let args = env::args().collect::<Vec<String>>();
-    let in_file: &str = match args.get(1) {
-        Some(filename) => filename,
-        None => "sample.asm",
+    let args = Cli::parse();
+    
+    let in_file = args.in_file_path;
+    let out_file = match args.out_file_path {
+        Some(filename) => filename.clone(),
+        None => {
+            let mut out_file = in_file.clone();
+            out_file.set_extension("hack");
+            out_file
+        },
     };
-    let out_file: &str = match args.get(2) {
-        Some(filename) => &filename,
-        None => "sample.hack",
-    };
-    println!("Reading file {}", in_file);
+    let dissassemble = args.disassemble;
 
     let in_file = std::fs::File::open(in_file)?;
     let out_file = std::fs::File::create(out_file)?;
